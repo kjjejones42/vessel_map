b0d345155c71f5529ca81193ceb98cc94b8fede1
Jonathan Jones <kjjejones42@gmail.com>
2024-06-08 16:28:07 +0100

    formatting change

--- frontend/lib/src/feature/vessel.dart
+++ frontend/lib/src/feature/vessel.dart
@@ -1,7 +1,6 @@
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Vessel {
-
   final int id;
   final String name;
   final LatLng location;
@@ -14,19 +13,18 @@ class Vessel {
 
   factory Vessel.fromJson(Map<String, dynamic> json) {
     return Vessel(
-      json['id'] as int,      
-      json['name'] as String,      
-      LatLng(json['latitude'] as double, json['longitude'] as double),
-      DateTime.parse(json['updated_at']).toLocal()
-    );
+        json['id'] as int,
+        json['name'] as String,
+        LatLng(json['latitude'] as double, json['longitude'] as double),
+        DateTime.parse(json['updated_at']).toLocal());
   }
 
   @override
   int get hashCode => Object.hash(id, name, location, updated);
 
   @override
-  bool operator == (Object other){
-    return hashCode == other.hashCode;  
+  bool operator ==(Object other) {
+    if (other.runtimeType != Vessel) return false;
+    return hashCode == other.hashCode;
   }
-
 }
