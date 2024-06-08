e5c8f37b1bc2ac82c63aaae8d35be8918623f035
Jonathan Jones <kjjejones42@gmail.com>
2024-06-08 10:34:25 +0100

    removed unneeded imports

--- frontend/lib/src/feature/app_model.dart
+++ frontend/lib/src/feature/app_model.dart
@@ -1,7 +1,4 @@
-import 'dart:collection';
-
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:vessel_map/src/feature/vessel.dart';
 
