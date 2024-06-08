35c66b554518b4ad607429e834d273027d96a91c
Jonathan Jones <kjjejones42@gmail.com>
2024-06-08 11:33:40 +0100

    renamed initialisation function

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -7,7 +7,7 @@ import 'package:universal_html/js.dart' as js;
 
 import 'src/app.dart';
 
-void init() async {
+void initialiseApp() async {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(
       ChangeNotifierProvider(create: (_) => AppModel(), child: const MyApp()));
@@ -18,11 +18,11 @@ void main() async {
   // the below code loads the key from the environment variables, sends it to the main page,
   // then continues to load the full app once the Maps script is loaded.
   if (kIsWeb && js.context['googleMapsLoaded'] != true) {
-    html.document.addEventListener('google-maps-loaded', (event) => init());
     var apiKey = const String.fromEnvironment('APIKEY', defaultValue: '');
+    html.document.addEventListener('google-maps-loaded', (event) => initialiseApp());
     html.document.dispatchEvent(
         html.CustomEvent('google-maps-api-key-loaded', detail: apiKey));
   } else {
-    init();
+    initialiseApp();
   }
 }
